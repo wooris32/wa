bb60b909dfab07423860feb9ae94c1524d0c2e66
D5319 <ksh32@gsretail.com>
2024-11-22 17:38:41 +0900

    1111

---
+++ src/main/java/wa/wooris/WoorisApplication.java
@@ -0,0 +1,13 @@
+package wa.wooris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoorisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wa/wooris/WoorisApplicationTests.java
@@ -0,0 +1,13 @@
+package wa.wooris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
